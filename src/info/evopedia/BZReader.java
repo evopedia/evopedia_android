09d6c3b68435794250eb8b061cc41cec35802458
peter <peter@exinto.de>
2012-12-19 00:05:47 -0300

    Catch the strange exception from CBZip2InputStream.

--- src/info/evopedia/BZReader.java
+++ src/info/evopedia/BZReader.java
@@ -6,23 +6,34 @@ import java.io.InputStream;
 
 import org.apache.tools.bzip2.CBZip2InputStream;
 
+import android.util.Log;
+
 public class BZReader {
 
     public static byte[] readAt(InputStream stream, long blockStart, long blockOffset, long dataLength)
                 throws IOException {
-        stream.skip(blockStart + 2); /* skip two header bytes */
-        CBZip2InputStream bzip = new CBZip2InputStream(new BufferedInputStream(stream, 20480));
-
-        for (long read = 0; read < blockOffset; ) {
-            read += bzip.skip(blockOffset - read);
+        if (stream == null) {
+            throw new NullPointerException("input stream is null");
         }
+        stream.skip(blockStart + 2); /* skip two header bytes */
 
         byte[] data = new byte[(int) dataLength];
-        for (int read = 0; read < dataLength; ) {
-            int r = bzip.read(data, read, data.length - read);
-            if (r == -1)
-                break;
-            read += r;
+        try {
+            CBZip2InputStream bzip = new CBZip2InputStream(new BufferedInputStream(stream, 20480));
+
+            for (long read = 0; read < blockOffset; ) {
+                read += bzip.skip(blockOffset - read);
+            }
+
+            for (int read = 0; read < dataLength; ) {
+                int r = bzip.read(data, read, data.length - read);
+                if (r == -1)
+                    break;
+                read += r;
+            }
+        } catch (NullPointerException exc) {
+            Log.d("BZReader", "Null pointer exception from CBZip2InputStream", exc);
+            return new byte[0];
         }
         return data;
     }
