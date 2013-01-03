07fbd8c18f90a33cce93c8eb0f80dcf04099292b
peter <peter@exinto.de>
2013-01-03 16:40:52 -0300

    Improved search dialog.

--- src/info/evopedia/EvopediaSearch.java
+++ src/info/evopedia/EvopediaSearch.java
@@ -1,14 +1,19 @@
 package info.evopedia;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
@@ -21,7 +26,10 @@ import com.actionbarsherlock.app.SherlockDialogFragment;
 public class EvopediaSearch extends SherlockDialogFragment
                 implements OnScrollListener, OnItemClickListener, TextWatcher {
 	private TitleAdapter titleAdapter;
+    private EditText titleSearch;
 	private ListView titleListView;
+
+	private String nextPrefix;
 
 	@Override
 	public void onAttach(Activity activity) {
@@ -30,30 +38,38 @@ public class EvopediaSearch extends SherlockDialogFragment
         titleAdapter = new TitleAdapter(activity);
 	}
 
-	@Override
-	public void onCreate(Bundle savedInstance) {
-	    super.onCreate(savedInstance);
-	    /* TODO this is still not really the style we want
-	     * - it hides the actionbar */
-	    setStyle(SherlockDialogFragment.STYLE_NO_FRAME, android.R.style.Theme);
+	public void setSearchPrefix(String prefix) {
+	    if (titleSearch == null) {
+	        nextPrefix = prefix;
+	    } else {
+	        titleSearch.setText(prefix);
+	    }
 	}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                   Bundle savedInstanceState) {
+        /* TODO use savedInstanceState */
         View view = inflater.inflate(R.layout.activity_evopedia_search,
                                      container, false);
+
+        titleSearch = (EditText) view.findViewById(R.id.titleSearch);
+        titleSearch.addTextChangedListener(this);
+        titleSearch.requestFocus();
 
         titleListView = (ListView) view.findViewById(R.id.titleListView);
         titleListView.setAdapter(titleAdapter);
         titleListView.setOnScrollListener(this);
         titleListView.setOnItemClickListener(this);
 
-        EditText titleSearch = (EditText) view.findViewById(R.id.titleSearch);
-        titleSearch.addTextChangedListener(this);
-        getDialog().getWindow().setSoftInputMode(
-                LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-        titleSearch.requestFocus();
+        if (nextPrefix != null) {
+            titleSearch.setText(nextPrefix);
+            titleAdapter.setPrefix(nextPrefix);
+            nextPrefix = null;
+        }
+
+        getDialog().requestWindowFeature(STYLE_NO_TITLE);
+
         return view;
     }
 
@@ -72,8 +88,8 @@ public class EvopediaSearch extends SherlockDialogFragment
 	@Override
 	public void afterTextChanged(Editable e) {
 		titleAdapter.setPrefix(e.toString());
-		/* TODO scrolling to origin does not work yet */
-		titleListView.scrollTo(0, 0);
+		/* scroll to top */
+		titleListView.setSelectionAfterHeaderView();
 	}
 
 	@Override
